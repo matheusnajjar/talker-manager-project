1afcb3aceceb5360f498bad53dc36c4b80dcc525
Matheus Najjar <matheusnajjar@gmail.com>
2022-08-02 19:05:13 -0300

    feat: adiciona rota /login e suas verificações

--- index.js
+++ index.js
@@ -1,11 +1,17 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 const fs = require('fs');
+const crypto = require('crypto');
 
 const talker = 'talker.json';
+const validEmail = /.{2,20}@.*\.com/;
 
 const app = express();
 app.use(bodyParser.json());
+
+function generateToken() {
+  return crypto.randomBytes(8).toString('hex');
+}
 
 const HTTP_OK_STATUS = 200;
 const PORT = '3000';
@@ -34,6 +40,25 @@ app.get('/talker/:id', (req, res) => {
 
   return res.status(200).json(selectedTalker); 
   });
+});
+
+app.post('/login', (req, res) => {
+  const { email, password } = req.body;
+  const token = generateToken();
+  if (!password) {
+    return res.status(400).json({ message: 'O campo "password" é obrigatório' });
+  }
+  if (password.length < 6) {
+    return res.status(400).json({ message: 'O "password" deve ter pelo menos 6 caracteres' });
+  }
+  if (!email) {
+    return res.status(400).json({ message: 'O campo "email" é obrigatório' });
+  }
+  if (!validEmail.test(email)) {
+    console.log(email, password);
+    return res.status(400).json({ message: 'O "email" deve ter o formato "email@email.com"' });
+  }
+  return res.status(200).json({ token });
 });
 
 app.listen(PORT, () => {
