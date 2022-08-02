cbb47495fa8f797412df81aaee9a847f8068a6bf
Matheus Najjar <matheusnajjar@gmail.com>
2022-08-02 17:17:36 -0300

    feat: cria rota /get e suas verificações

--- index.js
+++ index.js
@@ -1,5 +1,8 @@
 const express = require('express');
 const bodyParser = require('body-parser');
+const fs = require('fs');
+
+const talker = 'talker.json';
 
 const app = express();
 app.use(bodyParser.json());
@@ -10,6 +13,13 @@ const PORT = '3000';
 // não remova esse endpoint, e para o avaliador funcionar
 app.get('/', (_request, response) => {
   response.status(HTTP_OK_STATUS).send();
+});
+
+app.get('/talker', (_req, res) => {
+  fs.readFile(talker, 'utf-8', (_err, data) => {
+    if (data.length === 0) return [];
+    res.status(200).send(JSON.parse(data));
+  });
 });
 
 app.listen(PORT, () => {
